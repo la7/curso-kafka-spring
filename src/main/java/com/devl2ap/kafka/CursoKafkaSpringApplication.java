5a18d32d257b2882695087196a2e3d637e0f616c
leonardoarturoalpizarperez <usr.tmp1.mx@gmail.com>
2026-04-04 14:31:51 -0600

    Init repo

---
+++ src/main/java/com/devl2ap/kafka/CursoKafkaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.devl2ap.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoKafkaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoKafkaSpringApplication.class, args);
+	}
+
+}
