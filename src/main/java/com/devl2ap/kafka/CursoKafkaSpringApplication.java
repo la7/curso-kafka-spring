9fcd48e00200168a75311ac710df7fae2e70d548
leonardoarturoalpizarperez <usr.tmp1.mx@gmail.com>
2026-04-04 18:36:02 -0600

    Init Consumer & Producer

--- src/main/java/com/devl2ap/kafka/CursoKafkaSpringApplication.java
+++ src/main/java/com/devl2ap/kafka/CursoKafkaSpringApplication.java
@@ -1,13 +1,47 @@
 package com.devl2ap.kafka;
 
+import java.util.concurrent.CompletableFuture;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 
 @SpringBootApplication
-public class CursoKafkaSpringApplication {
+public class CursoKafkaSpringApplication implements CommandLineRunner {
 
+	private static final Logger logger = LoggerFactory.getLogger(CursoKafkaSpringApplication.class);
+	
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	@KafkaListener(topics = "l2ap-topic", groupId = "l2ap-group")
+	public void listen(String message) {
+		logger.warn("Received message: {}", message);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CursoKafkaSpringApplication.class, args);
 	}
+	@Override
+	public void run(String... args) throws Exception {
+		// 1. Cambia el tipo de retorno a CompletableFuture
+		CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send("l2ap-topic", "Sample Message Kafka L2AP");
 
+		// 2. Usa whenComplete en lugar de addCallback
+		future.whenComplete((result, ex) -> {
+			if (ex == null) {
+				// Caso de Éxito (Success)
+				logger.warn("Message sent successfully: {}", result.getRecordMetadata().offset());
+			} else {
+				// Caso de Error (Failure)
+				logger.error("Failed to send message: {}", ex.getMessage());
+			}
+		});
+
+	}
 }
---
+++ src/main/java/com/devl2ap/kafka/config/KafkaConfiguration.java
@@ -0,0 +1,60 @@
+package com.devl2ap.kafka.config;
+
+import java.util.Map;
+
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.HashMap;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+
+@Configuration
+public class KafkaConfiguration {
+    public Map<String, Object> producerProperties() {
+        // Configure the producer properties
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+                    
+        return props;
+    }
+
+    public Map<String, Object> consumerProperties() {
+        // Configure the producer properties
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "l2ap-group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+                    
+        return props;
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        DefaultKafkaProducerFactory<String, String> producerFactory = new DefaultKafkaProducerFactory<>(producerProperties());
+        KafkaTemplate<String, String> kafkaTemplate = new KafkaTemplate<>(producerFactory);
+        return kafkaTemplate;
+    }
+    
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerProperties());
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+}
