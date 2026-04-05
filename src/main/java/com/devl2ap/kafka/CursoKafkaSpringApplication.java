456de9a520050f55a4f7a6269d4847fc799e80c3
leonardoarturoalpizarperez <usr.tmp1.mx@gmail.com>
2026-04-04 20:20:19 -0600

    Start & stop

--- src/main/java/com/devl2ap/kafka/CursoKafkaSpringApplication.java
+++ src/main/java/com/devl2ap/kafka/CursoKafkaSpringApplication.java
@@ -12,6 +12,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 
@@ -23,7 +24,10 @@ public class CursoKafkaSpringApplication implements CommandLineRunner {
 	@Autowired
 	private KafkaTemplate<String, String> kafkaTemplate;
 
-	@KafkaListener(topics = "l2ap-topic", groupId = "l2ap-group", containerFactory = "kafkaListenerContainerFactory", properties = {"max.poll.interval.ms=4000","max.poll.records=10"})
+	@Autowired
+	private KafkaListenerEndpointRegistry registry;
+
+	@KafkaListener(id = "l2apId", autoStartup = "false",topics = "l2ap-topic", groupId = "l2ap-group", containerFactory = "kafkaListenerContainerFactory", properties = {"max.poll.interval.ms=4000","max.poll.records=10"})
 	public void listen(List<ConsumerRecord<String, String>> messages) {
 		logger.warn("Start reading BATCH");
 		for(ConsumerRecord<String, String> message : messages) {
@@ -40,6 +44,12 @@ public class CursoKafkaSpringApplication implements CommandLineRunner {
 		for(int i=0; i<100; i++) {
 			kafkaTemplate.send("l2ap-topic", String.valueOf(i),String.format("Sample Message %d", i));
 		}
+		logger.info("Espera de 5 segundos para que se procesen los mensajes...");
+		Thread.sleep(5000); // Espera a que se procesen los mensajes antes de cerrar la aplicación
+		logger.info("Listener iniciado, esperando mensajes...");
+		registry.getListenerContainer("l2apId").start(); // Inicia el listener
+		Thread.sleep(3000); // Mantiene la aplicación en ejecución para recibir mensajes
+		registry.getListenerContainer("l2apId").stop(); // Detiene el listener
 	}
 
 	/*ASYNC*/
